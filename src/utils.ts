83393e7293b4df4468668b1e518afb2e277b52c9
MaxLmqr <max@galadrim.fr>
2023-11-09 17:42:30 +0100

    feat(experimental): gpt review

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export const baseContent = `Keep in mind that you are here to help a lead developper revieweing a pull request from a developer. You will receive a GitHub patch file content.
+export const baseContent = `You will receive a GitHub patch file content. Keep in mind that you are here to help a lead developer revieweing a pull request from a developer.
 Answer with the sentence : "No comment" unless there is valuable information to share. 
 Pay attention to syntax, improvment, logic, performance, readability, maintainability, scalability, reusability, complexity, best practice, convention. 
 Do NOT explain what the code is doing. Be relevant. Review the following code and provide comments :\n`
