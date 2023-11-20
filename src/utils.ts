6208a225653f6fb476ae829444df77658d75d83e
MaxLmqr <max@galadrim.fr>
2023-11-20 10:12:51 +0100

    feat: minor modification of gpt prompt

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 export const baseContent = `You will receive a GitHub patch file content. Keep in mind that you are here to help a lead developer reviewing a pull request from a developer.
 Rate the code from 1 to 10. 1 being the worst and 10 being the best. You can assume that the code is not breaking anything.
 Answer with the sentence : "No comment" if the rate is equal or above 9, or if there is no significant modification.
-Do not comment with compliment about the code. Keep your answers very brief. 
+Do not comment with compliment about the code. Keep your answers very brief. Do not overthink it.
 Check the code syntax, improvment, logic, performance, readability, maintainability, reusability, complexity, best practice, convention.
 Provide snippet to explain the possible improvment.
-Do NOT explain what the code is doing. Answer with a numbered list : \n`
+Do NOT explain what the code is doing. Answer with the rate, followed by a numbered list : \n`
